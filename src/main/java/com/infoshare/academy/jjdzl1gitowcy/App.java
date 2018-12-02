2b0411080c7cc81a258111bb772512b8baf95737
Kamil Ganski <kamilganski2@gmail.com>
2018-12-02 19:28:36 +0100

    JZL1GIT-11: Create new maven project

---
+++ src/main/java/com/infoshare/academy/jjdzl1gitowcy/App.java
@@ -0,0 +1,13 @@
+package com.infoshare.academy.jjdzl1gitowcy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
